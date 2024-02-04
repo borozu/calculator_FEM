ddd40f1725d292d3e53d2f58266adade600d1b42
Kris <haski25@gmail.com>
2024-02-04 10:17:10 +0000

    comments, tidying up

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
+//create all necessary variables
 const input = document.getElementById("textAreaId");
-const c = document.getElementById("clearId");
-const back = document.getElementById("backId");
-const divide = document.getElementById("divideId");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
-const multiply = document.getElementById("multiplyId");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const minus = document.getElementById("minusId");
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const plus = document.getElementById("plusId");
-const zero = document.getElementById("0");
+const clear = document.getElementById("clearId");
+const backArrow = document.getElementById("backId");
 const equals = document.getElementById("equalsId");
+const allNumberButtons = document.querySelectorAll(".number-button");
+const allArithmetics = document.querySelectorAll(".arithmetic");
 
 //string which stores whole equasion + helper temporary string
 let equasion = "";
@@ -28,28 +17,25 @@ const noLeadingZeros = function (inputString) {
 };
 
 //clear inputbox and calculation strings
-c.addEventListener("click", () => {
+clear.addEventListener("click", () => {
   input.innerText = "";
   equasion = "";
   tempString = "";
 });
-back.addEventListener("click", () => {
+backArrow.addEventListener("click", () => {
   equasion = equasion.slice(0, -1);
   input.innerText = equasion;
 });
 
 //inputbox number is transfered to temporary string
-const allNumberButtons = document.querySelectorAll(".number-button");
 allNumberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     tempString = tempString + button.innerText;
     input.innerText = tempString;
-    console.log("tempstring: " + tempString);
   });
 });
 
 //arithmetics symbols bevahiour
-const allArithmetics = document.querySelectorAll(".arithmetic");
 allArithmetics.forEach((symbol) => {
   symbol.addEventListener("click", () => {
     equasion += noLeadingZeros(tempString) + symbol.innerText;
@@ -64,5 +50,4 @@ equals.addEventListener("click", () => {
   input.innerText = result;
   tempString = "";
   equasion = "";
-  console.log(result);
 });
